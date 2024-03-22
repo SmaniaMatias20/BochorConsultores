0fbf2a94e544b0399fa67f7a9de905fadaa46d43
Matias Smania <matiasezequielsmania@gmail.com>
2024-03-22 13:30:01 -0300

    version 1.0

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-document.getElementById('navbarToggler').addEventListener('click', function() {
-  document.getElementById('navbarMenu').classList.toggle('active');
+// Script para controlar el menú hamburguesa
+document.addEventListener('DOMContentLoaded', function () {
+  const hamburgerMenu = document.querySelector('.hamburger-menu');
+  const nav = document.querySelector('.lista-nav');
+
+  hamburgerMenu.addEventListener('click', function () {
+      nav.classList.toggle('show'); // Toggle añade o quita la clase 'show'
+  });
 });
+
+
