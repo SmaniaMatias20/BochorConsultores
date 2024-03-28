628b1e72125eea6d20867c28ed1ddd7ca87fcc9e
Matias <matiasezequielsmania@gmail.com>
2024-03-28 13:43:02 -0300

    version 1.0

--- js/script.js
+++ js/script.js
@@ -1,29 +1,43 @@
 // Script para controlar el menú hamburguesa
 document.addEventListener('DOMContentLoaded', function () {
-  const hamburgerMenu = document.querySelector('.hamburger-menu');
-  const nav = document.querySelector('.lista-nav');
+    const hamburgerMenu = document.querySelector('.hamburger-menu');
+    const nav = document.querySelector('.lista-nav');
 
-  hamburgerMenu.addEventListener('click', function () {
-      nav.classList.toggle('show'); // Toggle añade o quita la clase 'show'
-  });
-
-
-
-  // Animación para los enlaces del menú del encabezado
-  const listaNav = document.querySelector('.lista-nav');
-  listaNav.querySelectorAll('li').forEach(enlace => {
-      enlace.addEventListener('mouseenter', () => {
-          enlace.style.transform = 'scale(1.1)';
-      });
-      enlace.addEventListener('mouseleave', () => {
-          enlace.style.transform = 'scale(1)';
-      });
-  });
+    hamburgerMenu.addEventListener('click', function () {
+        nav.classList.toggle('show'); // Toggle añade o quita la clase 'show'
+    });
 
 
-  
-  
+
+    // Animación para los enlaces del menú del encabezado
+    const listaNav = document.querySelector('.lista-nav');
+    listaNav.querySelectorAll('li').forEach(enlace => {
+        enlace.addEventListener('mouseenter', () => {
+            enlace.style.transform = 'scale(1.1)';
+        });
+        enlace.addEventListener('mouseleave', () => {
+            enlace.style.transform = 'scale(1)';
+        });
+    });
+
+    var imagenes = document.querySelectorAll('.consejo-imagen');
+
+    // Agregar un evento de clic a cada imagen
+    imagenes.forEach(function (imagen) {
+        imagen.addEventListener('click', function () {
+            // Cambiar el tamaño de la imagen al hacer clic
+            this.classList.toggle('imagen-ampliada');
+        });
+    });
+
+
+
+
+
+
+
 });
+
 
 // confirm("Estas seguro que deseas salir del sitio web?");
 
