33bb2d315bf46924a6354e674683f134045d87fb
Matias <matiasezequielsmania@gmail.com>
2024-04-01 10:10:08 -0300

    version 1.0

--- js/script.js
+++ js/script.js
@@ -7,8 +7,6 @@ document.addEventListener('DOMContentLoaded', function () {
         nav.classList.toggle('show'); // Toggle añade o quita la clase 'show'
     });
 
-
-
     // Animación para los enlaces del menú del encabezado
     const listaNav = document.querySelector('.lista-nav');
     listaNav.querySelectorAll('li').forEach(enlace => {
@@ -20,16 +18,109 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 
-    var imagenes = document.querySelectorAll('.consejo-imagen');
+    // Animacion Scroll Index
+    
+    ScrollReveal().reveal('.banner-imagen', { 
+    delay: 100,
+    duration: 1000,
+    distance: '50px',
+    origin: 'bottom'
+    });
 
-    // Agregar un evento de clic a cada imagen
-    // imagenes.forEach(function (imagen) {
-    //     imagen.addEventListener('click', function () {
-    //         // Cambiar el tamaño de la imagen al hacer clic
-    //         this.classList.toggle('imagen-ampliada');
-    //     });
-    // });
+    ScrollReveal().reveal('.contenedor-articulo', { 
+    delay: 100,
+    duration: 1000,
+    distance: '50px',
+    origin: 'bottom'
+    });
 
+    ScrollReveal().reveal('.segundo-contenedor-articulo', { 
+    delay: 200,
+    duration: 1200,
+    distance: '50px',
+    origin: 'bottom'
+    });
+
+    ScrollReveal().reveal('.liderazgo', { 
+    delay: 300,
+    duration: 1400,
+    distance: '50px',
+    origin: 'bottom'
+    });
+
+    ScrollReveal().reveal('.publicidad', { 
+    delay: 400,
+    duration: 1600,
+    distance: '50px',
+    origin: 'bottom'
+    });
+
+    ScrollReveal().reveal('.recomendacion', { 
+    delay: 500,
+    duration: 1800,
+    distance: '50px',
+    origin: 'bottom'
+    });
+
+    // Animacion Scroll Service
+
+    ScrollReveal().reveal('.articulos-servicios', { 
+    delay: 100,
+    duration: 1000,
+    distance: '50px',
+    origin: 'bottom'
+    });
+
+    ScrollReveal().reveal('.sintoma', { 
+    delay: 200,
+    duration: 1000,
+    distance: '50px',
+    origin: 'bottom'
+    });
+
+    ScrollReveal().reveal('.articulos-politicas', { 
+    delay: 300,
+    duration: 1000,
+    distance: '50px',
+    origin: 'bottom'
+    });
+
+    // Animacion Scroll About
+
+    ScrollReveal().reveal('.card', { 
+    delay: 100,
+    duration: 1000,
+    distance: '50px',
+    origin: 'bottom'
+    });
+
+    ScrollReveal().reveal('.eleccion', { 
+    delay: 200,
+    duration: 1000,
+    distance: '50px',
+    origin: 'bottom'
+    });
+
+    ScrollReveal().reveal('.mision', { 
+    delay: 300,
+    duration: 1000,
+    distance: '50px',
+    origin: 'bottom'
+    });
+
+    ScrollReveal().reveal('.vision', { 
+    delay: 400,
+    duration: 1000,
+    distance: '50px',
+    origin: 'bottom'
+    });
+
+    ScrollReveal().reveal('.valores', { 
+    delay: 500,
+    duration: 1000,
+    distance: '50px',
+    origin: 'bottom'
+    });
 
 
 
@@ -38,7 +129,5 @@ document.addEventListener('DOMContentLoaded', function () {
 
 });
 
-
-// confirm("Estas seguro que deseas salir del sitio web?");
 
 
